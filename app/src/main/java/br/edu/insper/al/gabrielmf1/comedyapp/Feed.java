ad0284fd9bbd95db0800cd3e6e509aa97db031df
gabrielmf1 <gabrielmf1@al.insper.edu.br>
2020-05-27 16:12:12 -0300

    Login/Feed

---
+++ app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/Feed.java
@@ -0,0 +1,14 @@
+package br.edu.insper.al.gabrielmf1.comedyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
--- app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/MainActivity.java
+++ app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/MainActivity.java
@@ -1,7 +1,10 @@
 package br.edu.insper.al.gabrielmf1.comedyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonLogin = findViewById(R.id.login_button);
+        Button buttonFeed = findViewById(R.id.login_feed);
+
+
+        buttonLogin.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        });
+
+        buttonFeed.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, Feed.class);
+            startActivity(intent);
+        });
     }
 }
