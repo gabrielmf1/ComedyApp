20ec622185132f248c1378f332f4b1a86b9d155f
gabrielmf1 <gabrielmf1@al.insper.edu.br>
2020-05-27 15:52:46 -0300

    Bugs

--- app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/MainActivity.java
+++ app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/MainActivity.java
@@ -1,9 +1,7 @@
 package br.edu.insper.al.gabrielmf1.comedyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button buttonLogin = findViewById(R.id.login_button);
-        Button buttonFeed = findViewById(R.id.feed_button);
     }
 }
