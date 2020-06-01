2e38fa25ed7de4cf0b4434a0a3bf1ede3541ce57
gabrielmf1 <gabrielmf1@al.insper.edu.br>
2020-06-01 17:26:12 -0300

    Até agr

--- app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/Login.java
+++ app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/Login.java
@@ -3,12 +3,25 @@ package br.edu.insper.al.gabrielmf1.comedyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    EditText etEmail, etPass;
+    Button btnLogin;
+    TextView btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnReg = findViewById(R.id.btnReg);
     }
 }
