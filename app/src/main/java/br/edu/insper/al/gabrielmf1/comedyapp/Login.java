b75fe5ccc1de74f113800b2b07d5e1c3b7b56479
gabrielmf1 <gabrielmf1@al.insper.edu.br>
2020-06-01 17:55:19 -0300

    wow

--- app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/Login.java
+++ app/src/main/java/br/edu/insper/al/gabrielmf1/comedyapp/Login.java
@@ -3,15 +3,17 @@ package br.edu.insper.al.gabrielmf1.comedyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
     EditText etEmail, etPass;
     Button btnLogin;
-    TextView btnReg;
+    TextView tvReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,24 @@ public class Login extends AppCompatActivity {
         etPass = findViewById(R.id.etPass);
 
         btnLogin = findViewById(R.id.btnLogin);
-        btnReg = findViewById(R.id.btnReg);
+        tvReg = findViewById(R.id.tvReg);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,pass;
+
+                email = etEmail.getText().toString();
+                pass = etPass.getText().toString();
+
+                if (email.equals("")){
+                    Toast.makeText(Login.this, "Insira um Email válido", Toast.LENGTH_SHORT).show();
+                }
+                else if (etPass.equals("")){
+                    Toast.makeText(Login.this, "Insira um Email válido", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
