b0eae6c2253615793e1be934dbb54a2affcdc139
gabrielmf1 <gabrielmf1@al.insper.edu.br>
2020-06-15 16:30:13 -0300

    BlogApp

---
+++ app/src/main/java/br/edu/insper/al/gabrielmf1/mainactivity/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package br.edu.insper.al.gabrielmf1.mainactivity.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.insper.al.gabrielmf1.mainactivity.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
